68689f3dfa5ca6e92185efffb0e63e1223c1e408
Aleksander <141730118+janicaleksander@users.noreply.github.com>
2026-03-22 21:10:00 +0100

    fix: enhance database connection settings

--- decision-service/infrastructure/database.go
+++ decision-service/infrastructure/database.go
@@ -2,6 +2,7 @@ package infrastructure
 
 import (
 	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,5 +14,15 @@ func NewDB() (*gorm.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxOpenConns(10)
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+	sqlDB.SetConnMaxIdleTime(10 * time.Minute)
 	return db, nil
+
 }
