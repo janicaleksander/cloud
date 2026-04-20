6cac0cf8430e80626e2b1b64371a377397af8acf
Aleksander <141730118+janicaleksander@users.noreply.github.com>
2026-04-20 23:39:11 +0200

    feat: preparation for RDS

--- claim-service/infrastructure/database.go
+++ claim-service/infrastructure/database.go
@@ -10,15 +10,21 @@ import (
 	"gorm.io/gorm"
 )
 
+/*
+dodanie regult SG w ec2 do portu 5432
+wlaczeni w modify RDS public accessibility
+*/
 func NewDB() (*gorm.DB, error) {
 	slog.Info("Connecting to database for claim service")
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=prefer",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s sslrootcert=%s",
 		os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
 		os.Getenv("POSTGRES_DB"),
 		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_SSLMODE"),
+		os.Getenv("POSTGRES_SSLROOTCERT"),
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
