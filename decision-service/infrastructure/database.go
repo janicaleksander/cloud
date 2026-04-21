3b2b113af516fbaa56138d550df3b4a1b498a79f
Aleksander <141730118+janicaleksander@users.noreply.github.com>
2026-04-21 14:46:36 +0200

    feat: prepare aws RDS connections

--- decision-service/infrastructure/database.go
+++ decision-service/infrastructure/database.go
@@ -11,14 +11,16 @@ import (
 )
 
 func NewDB() (*gorm.DB, error) {
-	slog.Info("Connecting to database")
+	slog.Info("Connecting to database for decision service")
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s sslrootcert=%s",
 		os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
 		os.Getenv("POSTGRES_DB"),
 		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_SSLMODE"),
+		os.Getenv("POSTGRES_SSLROOTCERT"),
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
