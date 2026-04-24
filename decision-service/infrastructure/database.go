e603f5bf65020230a9eb5c69f721905d602afce7
Aleksander <141730118+janicaleksander@users.noreply.github.com>
2026-04-24 21:41:09 +0200

    db fixes

--- decision-service/infrastructure/database.go
+++ decision-service/infrastructure/database.go
@@ -13,12 +13,13 @@ import (
 func NewDB() (*gorm.DB, error) {
 	slog.Info("Connecting to database")
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
 		os.Getenv("POSTGRES_DB"),
 		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_SSLMODE"),
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
